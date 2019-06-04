7fa7d321920886032c60e4c8dfa61f62dea9efeb
josh182014 <josh182014@gmail.com>
2019-06-04 13:50:53 -0600

    set up zoo router

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const zoosRouter = require('./zoos/zoos-router.js')
+
 const server = express();
 
 server.use(express.json());
@@ -12,3 +14,5 @@ const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
+server.use('/api/zoos', zoosRouter);
---
+++ zoos/zoos-router.js
@@ -0,0 +1,10 @@
+const knex = require('knex')
+
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.status(200).json({message: "success"})
+})
+
+
+module.exports = router;
